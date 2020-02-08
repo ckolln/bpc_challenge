741f579d13083f53ae3da08affe14a834bd36bf6
Claire Kolln <ckolln55@gmail.com>
2020-02-07 16:28:41 -0800

    Completed coding challenge

---
+++ code-ex-bpc/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div class="skin1">
+      <skin1/>
+    </div>
+    <div class="skin2">
+    <skin2/>
+    </div>
+  </div>
+</template>
+
+<script>
+import skin1 from './components/skin1.vue'
+import skin2 from './components/skin2.vue'
+
+export default {
+  name: 'app',
+  components: {
+    skin1,
+    skin2
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  display:inline-flex;
+}
+
+.skin1 {
+  margin: 100px;
+  display: inline-flex;
+  overflow: visible;
+}
+
+.skin2 {
+  margin-top: 120px;
+  display: inline-flex;
+}
+
+html {
+  background-color: #d9e8e6;
+}
+</style>
---
+++ code-ex-bpc/src/components/skin2.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="hello">
+    <div class="autosearch">
+        <input v-model="query" v-on:keyup="autocomplete" type="text">
+      <div class="suggestions">
+        <div v-bind:key="index" v-for="(item,index) in results" v-bind:class="{'auto-item':true, 'active':(index === focus)}">
+          <strong>{{ item.name }}</strong>
+          <br>
+          {{ item.description }}
+          <br>
+          <div class="tag" v-bind:key="index" v-for="(tag,index) in item.tags">
+            <button>#{{ tag }}</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import axios from "axios";
+
+export default {
+  name: 'skin2',
+  data () {
+    return {
+      query: "",
+      results: [],
+      focus: -1,
+      length: 0,
+      populate: "",
+    }
+  },
+  methods: {
+    autocomplete: function(event) {
+      
+      // highlight the appropriate autocomplete-item
+      if (event.keyCode === 40 ) {
+        if(this.focus < (this.length-1)) {
+          this.focus++;
+        }
+        else {
+          this.focus = 0;
+        }
+      }
+      else if (event.keyCode === 38) {
+        if(this.focus > -1) {
+          this.focus--;
+        }
+        else {
+          this.focus = (this.length -1)
+        }
+      }
+      else if (event.keyCode === 13) {
+        // if user presses enter on highlighted item, populate search bar with the name
+        this.query = this.results[this.focus].name;
+        this.results = [];
+      }
+      else {
+        axios({ method:"GET", url:this.query, baseURL:"https://coding-challenge.echoandapex.com/locations?q="}).then(result => {
+        this.results = result.data.predictions;
+        this.length = result.data.predictions.length;
+        this.focus = -1;
+      }, 
+      )
+      }
+    },
+   },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.suggestions {
+  overflow: hidden; 
+  width: 320px;
+  position: relative;
+  border: 1px solid transparent;
+  border-top: none;
+  border-bottom: none;
+  border-radius: 0px 0px 25px 25px;
+  margin-left:15px;
+}
+
+.auto-item {
+  text-align: left;
+  background-color: white;
+  top: 100%;
+  padding: 10px;
+  width: 100%;
+  left: 0;
+  right: 0;
+  border-top: 1px solid gray;
+}
+
+.active {
+  background-color: #fff5f6;
+}
+
+input {
+  width: 350px;
+  font-size: 20px;
+  padding-bottom: 10px;
+  border-radius:16px;
+  padding:5px;
+}
+
+.hello {
+  text-align: center;
+  font-family: 'Open Sans', sans-serif;
+}
+
+.autosearch {
+  position: relative;
+  display: inline-block;
+}
+
+button {
+  background-color: pink;
+  padding: 10px 15px;
+  text-align: center;
+  display: inline-block;
+  border-radius: 16px;
+  position: relative;
+  margin: 5px;
+  font-size:12px;
+}
+.tag {
+  display: inline-block;
+};
+</style>
